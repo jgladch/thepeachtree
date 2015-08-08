723b6b71472ae8f8e3c24fe40a71a1a12795088b
jgladch <jeffgladchun@gmail.com>
2015-08-08 15:22:50 -0700

    Add Procfile and use ENV to set port

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 // server.js
-
-var express = require('express'),
-path = require('path'),
-app = express(),
-port = 3000,
-bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+var app = express();
+var port = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
 
 // Make sure to include the JSX transpiler
 require("node-jsx").install();
 
 // Include static assets. Not advised for production
 app.use(express.static(path.join(__dirname, 'public')));
+
 // Set view path
 app.set('views', path.join(__dirname, 'views'));
+
 // set up ejs for templating. You can use whatever
 app.set('view engine', 'ejs');
 
